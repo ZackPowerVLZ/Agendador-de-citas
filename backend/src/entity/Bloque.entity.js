7c343f27b7f3110adcbcb6b6c855ba79398b4987
VLZ <118009633+ZackPowerVLZ@users.noreply.github.com>
2025-08-08 13:40:51 -0400

    Create Bloque.entity.js

---
+++ backend/src/entity/Bloque.entity.js
@@ -0,0 +1,51 @@
+"use strict";
+import { EntitySchema } from "typeorm";
+
+const AgendaSchema = new EntitySchema({
+  name: "Bloque",
+  tableName: "bloques",
+  columns: {
+    id: {
+      type: "int",
+      primary: true,
+      generated: true,
+    },
+    tipo: {
+      type: "varchar",
+      length: 100,
+      nullable: false,
+    },
+    observacion: {
+      type: "text",
+      nullable: true,
+    },
+    fecha: {
+      type: "timestamp",
+      default: () => "CURRENT_TIMESTAMP",
+    },
+    estado: {
+      type: "varchar",
+      length: 20,
+      default: "pendiente",
+    },
+    descripcionResolucion: {
+      type: "text",
+      nullable: true,
+    },
+    visible: {
+      type: "boolean",
+      default: false,
+    },
+  },
+  relations: {
+    usuario: {
+      target: "User",
+      type: "many-to-one",
+      joinColumn: true,
+      eager: true,
+      nullable: false,
+    },
+  },
+});
+
+export default BloqueSchema;
